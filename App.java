6cb92c96bc460676ee9369496588bfbce9a98096
unknown <josephantonypulickal@gmail.com>
2023-12-12 00:55:25 +0530

    added first assignment

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Write a program to print two integer with the name of the integer.
+
+        int x=2;
+        int y=3;
+         System.out.println(x+": two");
+         System.out.println(y+": three");
+    }
+}
